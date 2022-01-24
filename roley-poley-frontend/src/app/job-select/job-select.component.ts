ae41d226cd915c768e72f9923bb3fc27c8c668b2
Grace Petryk <gracepetryk99@gmail.com>
2022-01-23 20:06:46 -0700

    implemented basic ui for specifying multiple jobs at different preference levels

--- roley-poley-frontend/src/app/job-select/job-select.component.ts
+++ roley-poley-frontend/src/app/job-select/job-select.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input, OnChanges, SimpleChanges, ChangeDetectorRef } from '@angular/core';
 import { Job } from '../types'
 import _jobs  from '../../assets/jobs.json';
 import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import {startWith, map} from 'rxjs/operators'
+import { BehaviorSubject, Observable } from 'rxjs';
+import {startWith, map, filter, observeOn} from 'rxjs/operators'
 import { JobSelect } from '../player-input/player-input.component';
 
 @Component({
@@ -16,36 +16,64 @@ export class JobSelectComponent implements OnInit {
   jobs: Job[] = _jobs;
   jobFormControl = new FormControl();
   jobFilterControl = new FormControl();
+  already_selected: Set<Job> = new Set();
   filteredJobs: Observable<Job[]>;
 
   @Input() id!: JobSelect;
-  @Output() jobSelectEvent = new EventEmitter<JobSelect>();
+  @Input() already_selected_observable?: BehaviorSubject<Set<Job>>;
+  @Output() jobChange = new EventEmitter<JobSelect>();
 
-  constructor() {
+  constructor(private cdrRef: ChangeDetectorRef) {
     this.filteredJobs = this.jobFilterControl.valueChanges.pipe(
       startWith(''),
-      map(job => (job ? this._filterJobs(job) : this.jobs))
+      map(job => (job ? this._filterJobsSearch(job) : this.jobs)),
     )
 
     this.jobFormControl.valueChanges.subscribe(
-      (job: string) => {
+      (job: Job) => {
         console.log(job)
-        this.jobSelectEvent.emit(this.id)
+        if (this.already_selected.has(job)) {
+          console.log("error, job already selected")
+        }
+        this.id.old_job = this.id.job
+        this.id.job = job
+        if (job !== undefined) {
+          this.jobChange.emit(this.id)
+        }
       }
     )
   }
 
-  private _filterJobs(value: string): Job[] {
+  private _filterJobsSearch(value: string): Job[] {
     const filterValue = value.toUpperCase()
 
-    return this.jobs.filter(job => job.short_name.startsWith(filterValue))
+    return this._filterJobsAlreadySelected().filter(job => job.short_name.startsWith(filterValue))
   }
 
-  disp_job(job: Job): string {
-    return job.short_name
+  private _filterJobsAlreadySelected(): Job[] {
+    return _jobs.filter(job => !this.already_selected.has(job))
   }
 
   ngOnInit(): void {
-      
+    // if we're given a job value by the parent update the selection
+    this.jobFormControl.setValue(
+      this.jobs.filter(job => job.short_name === this.id.job?.short_name)[0]
+    )
+
+    if (this.already_selected_observable !== null) {
+      this.already_selected_observable!.subscribe({
+        next: (job_set: Set<Job>) => {
+          console.log(this.id.pref_level, this.id.index)
+          this.already_selected = new Set(job_set)
+          this.already_selected.delete(this.jobFormControl.value)
+          this.jobs = this._filterJobsAlreadySelected()
+
+          // incredibly stupid hack to get the valueChanges observable to trigger
+          // there's got to be a better way to do this
+          this.jobFilterControl.setValue(' ')
+          this.jobFilterControl.setValue('')
+        }
+      })
+    }
   }
 }
--- roley-poley-frontend/src/app/player-input/player-input.component.ts
+++ roley-poley-frontend/src/app/player-input/player-input.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { ApplicationRef, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { JobSelectComponent } from '../job-select/job-select.component';
 import { Job } from '../types'
 
 export interface JobSelect {
-  name: string,
   pref_level: number,
   index: number,
-  job?: Job
+  job?: Job,
+  old_job?: Job
 }
 
 @Component({
@@ -17,4 +19,48 @@ export interface JobSelect {
 
 export class PlayerInputComponent {
   constructor () {}
+
+  already_selected: Set<Job> = new Set()
+  already_selected_observable: BehaviorSubject<Set<Job>> = new BehaviorSubject(this.already_selected)
+
+
+  job_choices: JobSelect[][] = [
+    [{
+      pref_level: 0,
+      index: 0,
+      job: undefined,
+      old_job: undefined
+    }]
+  ]
+
+  onJobChange($event: JobSelect) {
+
+    const pref_level = $event.pref_level
+    const index = $event.index
+    const job = $event.job!  // this handler does not fire if job is undefined
+    const old_job = $event.old_job
+
+    if (old_job !== undefined && this.already_selected.has(old_job)) {
+      this.already_selected.delete(old_job)
+    }
+    this.already_selected.add(job)
+    this.already_selected_observable.next(this.already_selected)
+
+
+    if (pref_level + 1 == this.job_choices.length) {
+      this.job_choices.push([{
+        pref_level: pref_level + 1,
+        index: 0,
+        job: undefined
+      }])
+    }
+
+    if (index + 1 == this.job_choices[pref_level].length) {
+      this.job_choices[pref_level].push({
+        pref_level: pref_level,
+        index: index + 1,
+        job: undefined
+      })
+    }
+  }
 }
